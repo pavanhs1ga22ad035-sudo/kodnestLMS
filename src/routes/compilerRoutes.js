03fab13771db998208f069f76fd49a8f450cde31
pavanhs1ga22ad035-sudo <pavan.h.s1ga22ad035@gmail.com>
2026-03-15 22:21:49 +0530

    Delete src directory

--- src/routes/compilerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authenticate } = require('../middlewares/authMiddleware');
-const { executeCode } = require('../controllers/compilerController');
-
-const router = express.Router();
-
-router.post('/execute', authenticate, executeCode);
-
-module.exports = router;

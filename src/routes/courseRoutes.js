c0b0177b96d6cda2c70ffb11e6f1b534a9ffa7fd
pavanhs1ga22ad035-sudo <pavan.h.s1ga22ad035@gmail.com>
2026-03-15 21:08:32 +0530

    Delete src directory

--- src/routes/courseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authenticate } = require('../middlewares/authMiddleware');
-const { getCoursePlayerData } = require('../controllers/courseController');
-
-const router = express.Router();
-
-router.get('/:courseId/player', authenticate, getCoursePlayerData);
-
-module.exports = router;
--- src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getHomeDashboard } = require('../controllers/dashboardController');
-const { authenticate } = require('../middlewares/authMiddleware');
-
-const router = express.Router();
-
-router.get('/home', authenticate, getHomeDashboard);
-
-module.exports = router;
